3bb222aa624eda0576f015f7548e876575e8f23f
Jace Browning <jacebrowning@gmail.com>
2016-12-29 20:50:17 -0500

    Add copy button to menu
    Closes #78

--- crowdsorter/views/collections.py
+++ crowdsorter/views/collections.py
@@ -56,7 +56,7 @@ def new():
     content, status = call(api.collections.create, name=name)
     assert status == 201
 
-    flash(f"Created collection: {content['name']}.", 'info')
+    flash(f"Created collection: {content['name']}", 'info')
 
     return redirect(url_for('admin.detail', key=content['key']))
 
