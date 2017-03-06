545b1646dec1d1e1c8795fc99350f5ec7b7992f5
Jace Browning <jacebrowning@gmail.com>
2017-03-05 19:45:02 -0500

    Update data generation for new fields

--- scripts/generate_sample_data.py
+++ scripts/generate_sample_data.py
@@ -4,7 +4,7 @@ import os
 
 from crowdsorter.settings import get_config
 from crowdsorter.factory import create_app
-from crowdsorter.models import Collection
+from crowdsorter.models import Collection, Item
 
 
 def main():
@@ -14,34 +14,40 @@ def main():
     collection.save()
 
     collection = Collection(name="New Collection", key='_new')
-    collection.items.append("Foo")
-    collection.items.append("Bar")
-    collection.items.append("A really long name with lots of words.")
-    collection.items.append("special_character:#")
-    collection.items.append("special_character:&")
-    collection.items.append("special_character:'")
+    collection.items2.append(Item(name="Foo").save())
+    collection.items2.append(Item(name="Bar").save())
+    collection.items2.append(Item(name="A really long name with lots of words.").save())
+    collection.items2.append(Item(name="special_character:#").save())
+    collection.items2.append(Item(name="special_character:&").save())
+    collection.items2.append(Item(name="special_character:'").save())
     collection.save()
 
     collection = Collection(name="Colors", key='_sample', code='sample',
                             private=True)
-    collection.items.append("Red")
-    collection.items.append("Green")
-    collection.items.append("Blue")
-    collection.items.append("Yellow")
+    collection.items2.append(Item(name="Red").save())
+    collection.items2.append(Item(name="Green").save())
+    collection.items2.append(Item(name="Blue").save())
+    collection.items2.append(Item(name="Yellow").save())
     collection.save()
 
     collection = Collection(name="Numbers", key='_numbers', code='numbers')
-    collection.items = [str(n) for n in range(1, 21)]
+    collection.items2 = [Item(name=str(n)).save() for n in range(1, 21)]
     collection.save()
 
     collection = Collection(name="Private Collection",
                             key='_private', private=True)
-    collection.items = ["Secret One", "Secret Two"]
+    collection.items2 = [
+        Item(name="Secret One").save(),
+        Item(name="Secret Two").save(),
+    ]
     collection.save()
 
     collection = Collection(name="Locked Collection",
                             key='_locked', locked=True)
-    collection.items = ["Item One", "Item Two"]
+    collection.items2 = [
+        Item(name="Item One").save(),
+        Item(name="Item Two").save(),
+    ]
     collection.save()
 
 
